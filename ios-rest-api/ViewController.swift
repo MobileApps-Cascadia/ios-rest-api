0e420db14936db7b19643e96774f5bddc18a826b
Student Account <student@mac-prod1.local>
2019-09-26 13:29:17 -0700

    codable protocol

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
     let DomainURL = "http://216.186.69.45/services/device/"
     
-    class User {
+class User: Codable {
+        
+        var UserID: String?
+        var FirstName: String?
+        var LastName: String?
+        var PhoneNumber: String?
+        var SID: String?
+        
         
         static func fetch(withId: Int){
             let URLstring = DomainURL + "users/\(withId)"
+            
             if let url = URL.init(string: URLstring){
                 let task = URLSession.shared.dataTask(with: url, completionHandler:
-                    { (apiData, urlResponse, error) in
+                    { (apiData, response, error) in
                         print(String.init(data: apiData!, encoding: .ascii) ?? "no data")
-                    }
-                )
+                        if let mUser = try? JSONDecoder().decode(User.self, from: apiData!) {
+                            
+                            print("\n\nLoading User...\nFirst Name: \(mUser.FirstName ?? "no first name on record")\nLast Name: \(mUser.LastName ?? "no last name on record")\nPhone Number: \(mUser.PhoneNumber ?? "no phone # on record")\nSID: \(mUser.SID ?? "no SID on record")")
+                        }
+                    })
                 task.resume()
             }
         }
