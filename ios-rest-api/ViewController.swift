77bc1513531b5e06cc3e2c1b538182b22799bee0
Student Account <student@mac-prod1.local>
2019-09-26 20:03:59 -0700

    finished the requirements of the assignment

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
     let DomainURL = "http://216.186.69.45/services/device/"
     
-    class User {
+class User : Codable {
+        var userID : String?
+        var firstName : String?
+        var lastName : String?
+        var phoneNumber : String?
+        var sid : String?
+    
+        enum CodingKeys : String, CodingKey{
+            case userID = "UserID"
+            case firstName = "FirstName"
+            case lastName = "LastName"
+            case phoneNumber = "PhoneNumber"
+            case sid = "SID"
+        }
         
-        static func fetch(){
-            let URLstring = DomainURL + "users/"
+        static func fetch(withID: Int){
+            let URLstring = DomainURL + "users/" + "\(withID)"
             if let url = URL.init(string: URLstring){
                 let task = URLSession.shared.dataTask(with: url, completionHandler:
-                    //TODO: Add closure
-                )
+                {(API_data, urlResponse, error) in
+                    print(String.init(data: API_data!, encoding: .ascii) ?? "no data")
+                    if let newUser = try? JSONDecoder().decode(User.self, from: API_data!){
+                        print(newUser.firstName ?? "no name")
+                    }
+                })
                 task.resume()
             }
         }
@@ -31,7 +48,7 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        User.fetch()
+        User.fetch(withID: 2)
     }
 
 
