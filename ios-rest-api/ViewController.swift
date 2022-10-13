4fbdb79b264b1c0ddda54ccf9886088d36c808ab
isaakwheeler <isaak.wheeler@hotmail.com>
2022-10-12 18:55:22 -0700

    re-did the code and got things working. Assignment complete.

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -8,20 +8,45 @@
 
 import UIKit
 
-    let DomainURL = "http://216.186.69.45/services/device/"
+let DomainURL = "https://mockend.com/MikeTheGreat/ios-rest-api-placeholder-data/"
+class User : Codable {
+    let id: Int?
+    let firstName: String?
+    let lastName: String?
+    let age: Int?
     
-    class User {
-        
-        static func fetch(){
-            let URLstring = DomainURL + "users/"
-            if let url = URL.init(string: URLstring){
-                let task = URLSession.shared.dataTask(with: url, completionHandler:
-                    //TODO: Add closure
-                )
-                task.resume()
-            }
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "FirstName"
+        case lastName = "LastName"
+        case age
+    }
+    
+    static func fetch(){
+        let URLstring = DomainURL + "users"
+        if let url = URL.init(string: URLstring){
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {
+                (data, response, error) in
+                print(String(data: data!, encoding: .utf8) ?? "No Data Recieved" as Any)
+            })
+            task.resume()
         }
     }
+    
+    static func fetch(withId: Int){
+        let URLstring = DomainURL + "users/\(String(withId))"
+        if let url = URL.init(string: URLstring){
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {
+                (data, response, error) in
+                print(String(data: data!, encoding: .utf8) ?? "No Data Recieved" as Any)
+                    if let newUser = try? JSONDecoder().decode(User.self, from: data!) {
+                    print(newUser.firstName ?? "No first name present" as Any)
+                }
+            })
+            task.resume()
+        }
+    }
+}
 
 class ViewController: UIViewController {
     
@@ -31,9 +56,10 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        User.fetch(withId: 1)
+        User.fetch(withId: 2)
         User.fetch()
     }
-
-
+    
+    
 }
-
